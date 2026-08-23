81642411e41b4d18dd40cd6e5b8964324c623bd7
ARYAN KUSHWAHA <a3018592@gmail.com>
2026-08-24 01:11:58 +0530

    Time: 0 ms (100.00%) | Memory: 117.9 MB (82.02%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode * reverse(ListNode* head){
+        ListNode* temp=NULL;
+        ListNode* prev=NULL;
+        ListNode* curr=head;
+
+        while(curr!=NULL){
+            temp=curr->next;
+            curr->next=prev;
+            prev = curr;
+            curr=temp;
+        }
+
+        return prev;
+    }
+    bool isPalindrome(ListNode* head) {
+        if(head==NULL || head->next==NULL) return true;
+
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast != NULL && fast->next != NULL ){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+
+        
+
+        ListNode* first=head;
+        ListNode* second=reverse(slow);;
+        while(second!=NULL){
+            if(first->val!=second->val){
+                second=reverse(slow);
+                return false;
+            }
+
+            first=first->next;
+            second=second->next;
+        }
+        second=reverse(slow);
+        return true;
+    }
+};
