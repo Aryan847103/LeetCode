0f67e7f796527e8eeab33b992f33d6f479033d5d
ARYAN KUSHWAHA <a3018592@gmail.com>
2026-09-12 02:05:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (48.38%) - LeetSync

---
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int cnt=0;
+        while(n){
+            if(n&1){
+                cnt++;
+            }
+            n=n>>1;
+
+            if(n==1){
+                 cnt++;
+                 break;
+            }
+        }
+        return cnt;
+    }
+};
